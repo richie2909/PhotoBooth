b0ea5071b9b804bb4750aaa2f341bf9275f67394
richie2909 <richiechristiandeguzman11@gmail.com>
2025-05-23 08:10:27 -0400

    wireframe1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 
 import HomeCanvas from '../components/HomeCanvas';
 import SnapCharmText from '../components/SnapCharmText';
