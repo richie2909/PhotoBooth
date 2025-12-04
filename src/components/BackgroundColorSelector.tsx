f5f65787bafcbc8f56ed187cb1fa0bf1c89240ea
zyvrixx <zyvrixx@gmail.com>
2025-12-04 08:27:16 +0800

    done adding event listener for chromepicker

--- src/components/BackgroundColorSelector.tsx
+++ src/components/BackgroundColorSelector.tsx
@@ -27,9 +27,9 @@ export default function backgroundColorSelector({ setBgColor,setShowPicker,showP
     console.log("outsideClick")
    }
   useEffect(() => {
-    document.addEventListener("mouseup", LostFocus)
+    document.addEventListener("mousedown", LostFocus)
     return () => {
- document.removeEventListener("mouseup", LostFocus)
+ document.removeEventListener("mousedown", LostFocus)
     } 
 
   }, [])
